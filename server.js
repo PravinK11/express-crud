dce1630f782c09f556d553928b9e1b81ed97bb90
PravinK11 <pravinrk1105@gmail.com>
2026-02-14 18:05:12 +0530

    updated api's with msql db

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require("express")
-// import mysql from 'mysql2/promise';
 const pool = require("./db")
 const app = express()
 const port = 5000
-
 app.use(express.json())
-
 app.get("/todo", async (req, res) => {
     try {
         const [rows] = await pool.query('select * from todo_app');
@@ -17,57 +14,46 @@ app.get("/todo", async (req, res) => {
         return res.status(500).send(error)
     }
 })
-
 app.post("/todo", async (req, res) => {
     try {
         const { todo, todo_desc, deadline, user_id } = req.body;
         if (!todo || !todo_desc || !deadline || !user_id) {
             return res.status(400).json({ message: "all fields required" })
         }
-
         const queryString = 'insert into todo_app (todo,todo_desc,deadline,user_id) values(?,?,?,?)';
         const queryValues = [todo, todo_desc, deadline, user_id];
         const result = await pool.query(queryString, queryValues);
-        console.log({result})
         return res.status(200).json({ message: "to do added successfully" })
     } catch (error) {
         console.log(error);
         return res.status(500).json({ message: "internal server error" }, error)
     }
 })
-
-app.put("/todo", (req, res) => {
+app.put("/todo/:id", async (req, res) => {
     try {
+        const todo_id = req.params.id;
+        console.log(todo_id);
         const body = req.body;
-        const { oldTodo, newTodo } = body;
-        const index = todos.indexOf(oldTodo);
-        if (index === -1) {
-            return res.status(400).send({ message: "todo not found" })
-        }
-        todos[index] = newTodo;
-        return res.status(200).send({ message: "todo updated successfully" }, todos)
+        const updated_todo = body.todo;
+        const queryString = 'update todo_app set todo=? where todo_id=?'
+        const queryValues = [updated_todo, todo_id]
+        const result = await pool.query(queryString, queryValues);
+        return res.status(200).json({ message: "todo updated successfully" }, result)
     } catch (error) {
         return res.status(500).send(error)
     }
 })
-app.delete("/todo", (req, res) => {
+app.delete("/todo/:id", async (req, res) => {
     try {
-        const body = req.body;
-        const rem = body.todo;
-        // console.log(rem);
-
-        const index = todos.indexOf(rem);
-        if (index === -1) {
-            return res.status(400).send({ message: "todo not found in array" })
-        }
-        todos.splice(index, 1)
+        const todo_id = req.params.id;
+        const queryString = 'delete from todo_app where todo_id=?'
+        const queryValues = [todo_id]
+        const result = await pool.query(queryString, queryValues);
         return res.status(200).send({ message: "todo deleted successfully" })
-        // console.log(todos)
     } catch (error) {
         return res.status(500).send(error)
     }
 })
-
 app.listen(port, () => {
     console.log(`app is listening to port ${port}`)
 })
