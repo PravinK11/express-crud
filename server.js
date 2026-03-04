cb87d7d1b6198abbc1528b3c94c8b4ba8f2e4e05
PravinK11 <pravinrk1105@gmail.com>
2026-03-04 17:23:08 +0530

    put api improved

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require("express")
+const cors = require("cors");
+const path = require("path");
+
+
 const pool = require("./db")
 const app = express()
 const port = 5000
+
+app.use(cors());
 app.use(express.json())
+app.use(express.static(path.join(__dirname, "TODO_UI1")));
+
 app.get("/todo", async (req, res) => {
     try {
         const [rows] = await pool.query('select * from todo_app');
@@ -16,6 +24,7 @@ app.get("/todo", async (req, res) => {
 })
 app.post("/todo", async (req, res) => {
     try {
+        console.log(req.body)
         const { todo, todo_desc, deadline, user_id } = req.body;
         if (!todo || !todo_desc || !deadline || !user_id) {
             return res.status(400).json({ message: "all fields required" })
@@ -32,13 +41,13 @@ app.post("/todo", async (req, res) => {
 app.put("/todo/:id", async (req, res) => {
     try {
         const todo_id = req.params.id;
-        console.log(todo_id);
-        const body = req.body;
-        const updated_todo = body.todo;
-        const queryString = 'update todo_app set todo=? where todo_id=?'
-        const queryValues = [updated_todo, todo_id]
+        const { todo, todo_desc, deadline, priority, todo_status } = req.body;
+        
+        const queryString = 'update todo_app set todo=?,todo_desc = ?,deadline = ?,priority = ?,todo_status = ? where todo_id=?'
+        const queryValues = [todo, todo_desc,deadline,priority,todo_status, todo_id]
         const result = await pool.query(queryString, queryValues);
         return res.status(200).json({ message: "todo updated successfully" }, result)
+
     } catch (error) {
         return res.status(500).send(error)
     }
@@ -54,6 +63,8 @@ app.delete("/todo/:id", async (req, res) => {
         return res.status(500).send(error)
     }
 })
+
+
 app.listen(port, () => {
     console.log(`app is listening to port ${port}`)
 })
