dd47f30727edc59af086dee68ae1c75af207b398
PravinK11 <pravinrk1105@gmail.com>
2026-03-15 17:44:49 +0530

    setup dotenv and environment variables

--- db.js
+++ db.js
@@ -1,17 +1,19 @@
 
-const mysql = require('mysql2/promise');
+import  mysql from 'mysql2/promise';
 
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const pool = mysql.createPool(
     {
-        host: "localhost",
-        user: "root",
-        password: "root",
-        database: "crud_app",
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
         waitForConnections: true,
         connectionLimit: 10,
         queueLimit: 0
     }
 );
-
-module.exports = pool;
+export default pool;
--- server.js
+++ server.js
@@ -1,15 +1,18 @@
-const express = require("express")
-const cors = require("cors");
-const path = require("path");
 
+import express from 'express';
+import cors  from "cors";
+import path from "path";
+import dotenv from 'dotenv';
 
-const pool = require("./db")
+dotenv.config();
+
+import  pool from "./db.js"
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json())
-app.use(express.static(path.join(__dirname, "TODO_UI1")));
+// app.use(express.static(path.join(__dirname, "TODO_UI1")));
 
 app.get("/todo", async (req, res) => {
     try {
